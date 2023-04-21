c8d24d425af1e8c25befe842b723456dfb115014
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-20 23:36:17 -0400

    Implemented CircuitBreaker

--- src/main/java/com/williamfeliciano/orderservice/external/client/PaymentService.java
+++ src/main/java/com/williamfeliciano/orderservice/external/client/PaymentService.java
@@ -1,7 +1,9 @@
 package com.williamfeliciano.orderservice.external.client;
 
+import com.williamfeliciano.orderservice.external.exception.CustomException;
 import com.williamfeliciano.orderservice.external.request.PaymentRequest;
 import com.williamfeliciano.orderservice.external.response.PaymentResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external",fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
@@ -17,4 +20,8 @@ public interface PaymentService {
 
     @GetMapping("/order/{orderId}")
     ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable("orderId") long orderId);
+
+    default void fallback(Exception e){
+        throw new CustomException("Payment Service is down","Payment Service Unavailable",503);
+    }
 }
--- src/main/java/com/williamfeliciano/orderservice/external/client/ProductService.java
+++ src/main/java/com/williamfeliciano/orderservice/external/client/ProductService.java
@@ -1,6 +1,8 @@
 package com.williamfeliciano.orderservice.external.client;
 
+import com.williamfeliciano.orderservice.external.exception.CustomException;
 import com.williamfeliciano.orderservice.external.response.ProductResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external",fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
@@ -16,4 +19,8 @@ public interface ProductService {
 
     @GetMapping("/{id}")
     ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId);
+
+    default void fallback(Exception e){
+        throw new CustomException("Product Service is down","Product Service Unavailable",503);
+    }
 }
